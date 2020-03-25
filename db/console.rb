8ded505160c9095b9c3bd783f5695eddfc3e7924
Kira Marston <kiramarston@gmail.com>
2020-03-25 20:00:38 +0000

    I did not git commit regularly. :(

---
+++ db/console.rb
@@ -0,0 +1,46 @@
+require("pry")
+require_relative("../models/album")
+require_relative("../models/artist")
+
+# 
+# Album.delete_all()
+# Artist.delete_all()
+#
+# artist1 = Artist.new({
+#   'name' => "Lady Gaga"
+#   })
+#
+# artist1.save()
+#
+# artist2 = Artist.new({
+#   'name' => "The Venga Boys"
+#   })
+#
+# artist2.save()
+#
+# album1 = Album.new({
+#   'title' => "Born This Way",
+#   'genre' => "Pop",
+#   'artist_id' => artist1.id
+#   })
+#
+# album1.save()
+#
+# album2 = Album.new({
+#   'title' => "Artpop",
+#   'genre' => "Pop",
+#   'artist_id' => artist1.id
+#   })
+#
+# album2.save()
+#
+# artist1.name = "Ariana Grande"
+# artist1.update()
+#
+
+
+
+
+
+binding.pry
+nil
